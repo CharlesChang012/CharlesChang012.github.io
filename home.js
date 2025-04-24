e6921953618c53ae6d17c835936e89d1eb7b21ed
Po-Hsun Chang <www.tw.charles@gmail.com>
2025-04-24 11:48:25 -0400

    Recommit

---
+++ home.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $("#home_photo").load(function() {
+        $(this).addClass("loaded");
+    });
+
+
+    $("#name").load(function() {
+        $(this).addClass("loaded");
+    });
+
+});
